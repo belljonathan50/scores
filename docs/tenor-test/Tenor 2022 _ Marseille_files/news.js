4481f105f220f034850574014e0c55d879932206
belljonathan50 <belljonathan50@gmail.com>
2021-08-18 11:36:36 +0200

    Add files via upload

---
+++ docs/tenor-test/Tenor 2022 _ Marseille_files/news.js
@@ -0,0 +1,2 @@
+document.write('<div class = "titre">latest news</div><br />');
+document.write('<p>    <a id="cv" href= http://localhost:8000/keynotes.html> Craig Vear Keynote </a>    </p>');
---
+++ docs/tenor-test/Tenor 2022 _ Marseille_files/spons.js
@@ -0,0 +1,13 @@
+document.write('<br />');
+document.write('<div class="titre">Support/Sponsors</div>');
+
+
+
+document.write('<a href="https://cordis.europa.eu/project/id/101002086" target ="_blank"><img src="http://localhost:8000/images/logos_org/eu.png" width = 180 </img></a> <br /> <br />');
+
+document.write('<br /><center>');
+// document.write('</div> <!-- navigation -->');
+
+
+
+
