93a028167a0d0c49f3a210c254e5ed7f9588b5fe
belljonathan50 <belljonathan50@gmail.com>
2021-08-23 00:23:11 +0200

    Update spons.js

--- docs/tenor-test/js/spons.js
+++ docs/tenor-test/js/spons.js
@@ -3,7 +3,7 @@ document.write('<div class="titre">Support/Sponsors</div>');
 
 
 
-document.write('<a href="https://cordis.europa.eu/project/id/101002086" target ="_blank"><img src="http://localhost:8000/img/logos_org/eu.png" width = 180 </img></a> <br /> <br />');
+document.write('<a href="https://cordis.europa.eu/project/id/101002086" target ="_blank"><img src="https://score.jonathanbell.eu/tenor-test/img/logos_org/eu.png" width = 180 </img></a> <br /> <br />');
 
 document.write('<br /><center>');
 // document.write('</div> <!-- navigation -->');
