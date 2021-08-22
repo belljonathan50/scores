f3054b11219b2e7d944adbc8a5cd4a2fd9295e5e
belljonathan50 <belljonathan50@gmail.com>
2021-08-22 23:18:55 +0200

    Add files via upload

--- docs/tenor-test/js/main.js
+++ docs/tenor-test/js/main.js
@@ -1,14 +1,34 @@
-console.log("gsapWorks");
+var tl = gsap.timeline();
+tl.to("#svg0", {delay: 3, duration: 0.3,x: 340, y:60});
+tl.to("#svg0", {duration: 0.3, rotation: -90,x: 1100, y:-500, scale: 
+1.3 });
+
+
+
+
+gsap.from("#camille", {ease:
+    Expo. easeIn, delay: 8, duration: 5, opacity: 0});
+
+gsap.from("#prism3d", { ease:
+    Expo. easeIn, duration: 12, opacity: 0});
+
+
+    gsap.to("#clef", {duration: 0.2 ,x: -700, y: -840});
+    gsap.to("#clef", {delay:3, duration: 1 , opacity: 0});
+
 gsap.to(".logo", {duration: 1,opacity: 0.2, x: 120, width:400, top:200});
 
 gsap.to("#hell", {duration: 1, bottom: "200px"});
+
+
+
 
 gsap.from("#digit", {duration: 4, opacity: 0});
 
 
 
-gsap.from("#note", {duration: 1,  x: 600, ease: "bounce"});
-gsap.to("#note", { opacity: 0, delay: 0.5});
+gsap.from("#note", {delay: 0.5, duration: 1,  x: 600, ease: "bounce"});
+gsap.to("#note", { opacity: 0, delay: 1});
 
 gsap.from("#note2", {duration: 1,  x: 600, delay: 1, ease: "bounce"});
 gsap.to("#note2", { opacity: 0, delay: 1.5});
@@ -16,10 +36,10 @@ gsap.to("#note2", { opacity: 0, delay: 1.5});
 gsap.from("#note3", {duration: 1,  x: 600, delay: 2, ease: "bounce"});
 gsap.to("#note3", { opacity: 0, delay: 2.5});
 
-gsap.from("#note4", {duration: 1,  x: 600, delay: 3, ease: "bounce"});
-gsap.to("#note4", { opacity: 0, delay: 3.5});
+gsap.from("#note4", {duration: 1,  x: 600, delay: 2.4, ease: "bounce"});
+gsap.to("#note4", { opacity: 0, delay: 3});
 
-gsap.from("#note5", {duration: 1,  x: 600, delay: 5, ease: "bounce"});
+gsap.from("#note5", {duration: 3,  x: 600, y: 50, scale:8, delay: 4, ease: "bounce"});
 gsap.to("#note5", { opacity: 0, delay: 5.5});
 
 gsap.from("#note6", {duration: 1,  x: 600, delay: 6, ease: "bounce"});
@@ -31,4 +51,5 @@ gsap.to("#note7", { opacity: 0, delay: 7.5});
 gsap.from("#note8", {duration: 1,  x: 600, delay: 8, ease: "bounce"});
 gsap.to("#note8", { opacity: 0, delay: 8.5});
 
-gsap.from(".bullet", {duration: .3, opacity: 0, x:-80, stagger: 0.4});
+gsap.from(".bullet", {delay: 4, duration: .2, opacity: 0, x:-80, stagger: 0.2});
+gsap.from(".bull", {delay: 4, duration: 1, opacity: 1, r:30, stagger: -1});
---
+++ docs/tenor-test/js/news.js
@@ -0,0 +1,25 @@
+document.write('<div class = "titre">latest news</div><br />');
+document.write('<p id="cv0">    <a id="cv" href="http://localhost:8000/news.html"> </a>_  </p>');
+
+let a = document.getElementById("cv");
+let b = document.getElementById("cv2");
+setTimeout(function(){ a.append("C"); }, 450);
+setTimeout(function(){ a.append("r"); }, 500);
+setTimeout(function(){ a.append("a"); }, 550);
+setTimeout(function(){ a.append("i"); }, 600);
+setTimeout(function(){ a.append("g"); }, 650);
+setTimeout(function(){ a.append(" "); }, 700);
+setTimeout(function(){ a.append("V"); }, 750);
+setTimeout(function(){ a.append("e"); }, 800);
+setTimeout(function(){ a.append("a"); }, 850);
+setTimeout(function(){ a.append("r"); }, 900);
+setTimeout(function(){ a.append(" "); }, 950);
+setTimeout(function(){ a.append("K"); }, 1000);
+setTimeout(function(){ a.append("e"); }, 1050);
+setTimeout(function(){ a.append("y"); }, 1100);
+setTimeout(function(){ a.append("n"); }, 1150);
+setTimeout(function(){ a.append("o"); }, 1200);
+setTimeout(function(){ a.append("t"); }, 1250);
+setTimeout(function(){ a.append("e"); }, 1300);
+
+
