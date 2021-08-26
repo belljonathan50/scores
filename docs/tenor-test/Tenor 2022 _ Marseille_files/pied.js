d020c76f1b4e235aa9c5990034d46a57b8fcb689
belljonathan50 <belljonathan50@gmail.com>
2021-08-26 15:55:20 +0200

    Delete pied.js

--- docs/tenor-test/Tenor 2022 _ Marseille_files/pied.js
+++
@@ -1,4 +0,0 @@
-document.write('<div id="pied">');
-document.write('<br />&copy; Copyright Tenor 2022. All rights reserved.<br />');
-document.write('</center>');
-document.write('</div><!-- #pied -->');
