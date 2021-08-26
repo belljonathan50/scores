219e76757ffc25fc439dfb81a4e7648d68f4c208
belljonathan50 <belljonathan50@gmail.com>
2021-08-26 15:54:23 +0200

    Delete writemail.js

--- docs/tenor-test/Tenor 2022 _ Marseille_files/writemail.js
+++
@@ -1,9 +0,0 @@
-function writemail(hostname,username,linktext,mode) {
-  if (hostname.length > 0) {
-	  mail_to="mail" + "to:" + username + "@" + hostname;
-	  if (mode==0) {  
-		  (linktext.length > 0)?document.write("<a href=" + mail_to + ">" + linktext + "</a>"):document.write("<a href=" +  mail_to + ">" + username + ""+"@" + hostname +"" + "</a>"); 
-	  }
-	  else { document.location.replace(mail_to); }
-  }
-}
