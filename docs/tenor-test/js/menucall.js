8f506af782d6a530ff52f48f05e6b2370b62d7ad
belljonathan50 <belljonathan50@gmail.com>
2021-08-28 23:39:16 +0200

    Create menucall.js

---
+++ docs/tenor-test/js/menucall.js
@@ -0,0 +1,24 @@
+document.write('<div id=navigation>');
+document.write('<ul>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a href ="main.html">    Welcome !             </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a href ="https://tenor2022.prism.cnrs.fr/index.html">     News                  </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a href ="https://tenor2022.prism.cnrs.fr/contribution.html">    Call for contributions</a></li>');
+
+document.write('<ul><li><img src="images/bullets/green.gif">  &nbsp; <a href="paper.html">Papers</a></li><li><img src="images/bullets/orange.gif"> &nbsp; <a href="demo.html">Demos and Workshops </a></li><li><img src="images/bullets/pink.gif">   &nbsp; <a href="music.html">Music </a></li></ul>');
+
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/sponsors.html">     Call for sponsors</a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/committee.html">Committee             </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/keynotes.html"> Keynotes               </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/special sessions.html"> Special sessions      </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/workshops.html"> Workshops     </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/program.html">  Program 				 </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/artistic program.html">  Artistic Program 				 </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/presenter instructions.html"> Presentation instructions</a></li>');  
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/registration.html"> Registration       </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/Important Dates.html">    Important dates        <a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/Author instructions & Submission.html">   Author instructions & Submission</a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/Conference venue.html">   Conference venue      </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/Accommodation.html"> Accommodation     </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="https://tenor2022.prism.cnrs.fr/history.html"> Tenor History</a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a href ="https://tenor2022.prism.cnrs.fr/contact.html">  Contact us            </a></li>');
+document.write('</ul>');
