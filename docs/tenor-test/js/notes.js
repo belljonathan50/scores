b4c158f9b290b8b9c0d7ee375cf15b682ea2a71b
belljonathan50 <belljonathan50@gmail.com>
2021-08-28 15:10:14 +0200

    Update notes.js

--- docs/tenor-test/js/notes.js
+++ docs/tenor-test/js/notes.js
@@ -1,23 +1,23 @@
-// gsap.from("#note", {delay: 0.5, duration: 1,  x: 600, ease: "bounce"});
-// gsap.to("#note", { opacity: 0, delay: 1});
+gsap.from("#note", {delay: 0.5, duration: 1,  x: 600, ease: "bounce"});
+gsap.to("#note", { opacity: 0, delay: 1});
 
-// gsap.from("#note2", {duration: 1,  x: 600, delay: 1, ease: "bounce"});
-// gsap.to("#note2", { opacity: 0, delay: 1.5});
+gsap.from("#note2", {duration: 1,  x: 600, delay: 1, ease: "bounce"});
+gsap.to("#note2", { opacity: 0, delay: 1.5});
 
-// gsap.from("#note3", {duration: 1,  x: 600, delay: 2, ease: "bounce"});
-// gsap.to("#note3", { opacity: 0, delay: 2.5});
+gsap.from("#note3", {duration: 1,  x: 600, delay: 2, ease: "bounce"});
+gsap.to("#note3", { opacity: 0, delay: 2.5});
 
-// gsap.from("#note4", {duration: 1,  x: 600, delay: 2.4, ease: "bounce"});
-// gsap.to("#note4", { opacity: 0, delay: 3});
+gsap.from("#note4", {duration: 1,  x: 600, delay: 2.4, ease: "bounce"});
+gsap.to("#note4", { opacity: 0, delay: 3});
 
-// gsap.from("#note5", {duration: 3,  x: 600, y: 50, scale:8, delay: 4, ease: "bounce"});
-// gsap.to("#note5", { opacity: 0, delay: 5.5});
+gsap.from("#note5", {duration: 3,  x: 600, y: 50, scale:8, delay: 4, ease: "bounce"});
+gsap.to("#note5", { opacity: 0, delay: 5.5});
 
-// gsap.from("#note6", {duration: 1,  x: 600, delay: 6, ease: "bounce"});
-// gsap.to("#note6", { opacity: 0, delay: 6.5});
+gsap.from("#note6", {duration: 1,  x: 600, delay: 6, ease: "bounce"});
+gsap.to("#note6", { opacity: 0, delay: 6.5});
 
-// gsap.from("#note7", {duration: 1,  x: 600, delay: 7, ease: "bounce"});
-// gsap.to("#note7", { opacity: 0, delay: 7.5});
+gsap.from("#note7", {duration: 1,  x: 600, delay: 7, ease: "bounce"});
+gsap.to("#note7", { opacity: 0, delay: 7.5});
 
-// gsap.from("#note8", {duration: 1,  x: 600, delay: 8, ease: "bounce"});
-// gsap.to("#note8", { opacity: 0, delay: 8.5});
+gsap.from("#note8", {duration: 1,  x: 600, delay: 8, ease: "bounce"});
+gsap.to("#note8", { opacity: 0, delay: 8.5});
