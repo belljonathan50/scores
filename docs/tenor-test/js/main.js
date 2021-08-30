3059425ea24ea3a64aff443b6f65f84d4d77e3ca
belljonathan50 <belljonathan50@gmail.com>
2021-08-30 23:09:48 +0200

    Update main.js

--- docs/tenor-test/js/main.js
+++ docs/tenor-test/js/main.js
@@ -1,42 +1,43 @@
+
+
+
 var tl = gsap.timeline();
+tl.to("#sandeep", {duration: 0.6,marginLeft: 60});
 tl.to("#svg0", {delay: 3, duration: 0.3,x: 340, y:60});
-tl.to("#svg0", {duration: 0.3, rotation: -90,x: 850, y:-200, scale: 
-0.9});
-
-
-gsap.to("#prismlogo", { ease:Expo. easeIn, duration: 1, opacity: 1});
-gsap.from("#prismlogo", {duration:2, rotateX: "random(-100,300)", rotateY: "random(-300,700)", rotateZ: "random(-200,400)"});
-
-
-gsap.to("#cnrslogo", { ease:Expo. easeIn, delay :0.5, duration: 0.5,  opacity: 1});
-gsap.to("#amulogo", { ease:Expo. easeIn, delay :0.7, duration: 0.5,  opacity: 1});
-gsap.to("#inciamlogo", { ease:Expo. easeIn, delay :0.9, duration: 0.5,  opacity: 1});
-gsap.to("#gmemlogo", { ease:Expo. easeIn, delay :1.1, duration: 0.5,  opacity: 1});
+tl.to("#svg0", {duration: 0.3, rotation: -90,x: -120, y:-550, scale: 0.4});
 
 
 
- gsap.from("#ent", {ease:
-    Expo. easeIn, duration: 0.5, opacity: 0});
+// gsap.to("#prismlogo", { ease:Expo. easeIn, duration: 1, opacity: 1});
+// gsap.from("#prismlogo", {duration:2, rotateX: "random(-100,300)", rotateY: "random(-300,700)", rotateZ: "random(-200,400)"});
+
+
+// gsap.to("#cnrslogo", { ease:Expo. easeIn, delay :0.5, duration: 0.5,  opacity: 1});
+// gsap.to("#amulogo", { ease:Expo. easeIn, delay :0.7, duration: 0.5,  opacity: 1});
+// gsap.to("#inciamlogo", { ease:Expo. easeIn, delay :0.9, duration: 0.5,  opacity: 1});
+// gsap.to("#gmemlogo", { ease:Expo. easeIn, delay :1.1, duration: 0.5,  opacity: 1});
+
+// gsap.to("#prismlogo", { ease:Expo. easeIn, duration: 0, opacity: 1});
 
 
 
- gsap.from("#trailerframe", {ease:
-    Expo. easeIn, duration: 1, opacity: 0});
+// gsap.to("#cnrslogo", { ease:Expo. easeIn,  opacity: 1});
+
+// gsap.to("#inciamlogo", { ease:Expo. easeIn,  opacity: 1});
+// gsap.to("#gmemlogo", { ease:Expo. easeIn,  opacity: 1});
 
 
-    
 
 
 
-gsap.from("#hope", {ease:
-    Expo. easeIn, delay: 1, duration: 3, opacity: 0});
 
 
-gsap.from("#camille", {ease:
-    Expo. easeIn, delay: 3, duration: 3, opacity: 0});
+// gsap.from("#craigtenor", {ease:
+//     Expo. easeIn, delay: 1, duration: 3, opacity: 0});
 
-gsap.from("#prism3d", { ease:
-    Expo. easeIn, duration: 3, opacity: 0});
+
+
+
 
 
     gsap.to("#clef", {duration: 0.2 ,x: -700, y: -840});
