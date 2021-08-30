c68ce2becc47befe5c5b8904d845bf35ad9a7353
belljonathan50 <belljonathan50@gmail.com>
2021-08-30 23:09:44 +0200

    Update menucall.js

--- docs/tenor-test/js/menucall.js
+++ docs/tenor-test/js/menucall.js
@@ -1,24 +1,24 @@
 document.write('<div id=navigation>');
 document.write('<ul>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a href ="main.html">    Welcome !             </a></li>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a href ="./index.html">     News                  </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a href ="index.html">    Welcome !             </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a href ="./news.html">     News                  </a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a href ="./contribution.html">    Call for contributions</a></li>');
 
 document.write('<ul><li><img src="images/bullets/green.gif">  &nbsp; <a href="paper.html">Papers</a></li><li><img src="images/bullets/orange.gif"> &nbsp; <a href="demo.html">Demos and Workshops </a></li><li><img src="images/bullets/pink.gif">   &nbsp; <a href="music.html">Music </a></li></ul>');
 
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="./sponsors.html">     Call for sponsors</a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="./committee.html">Committee             </a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a  href ="./keynotes.html"> Keynotes               </a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a  href ="./special sessions.html"> Special sessions      </a></li>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a  href ="./workshops.html"> Workshops     </a></li>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="./program.html">  Program 				 </a></li>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="./artistic program.html">  Artistic Program 				 </a></li>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a  href ="./presenter instructions.html"> Presentation instructions</a></li>');  
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a  href ="./registration.html"> Registration       </a></li>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a  href ="./Important Dates.html">    Important dates        <a></li>');
+// document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a  href ="./workshops.html"> Workshops     </a></li>');
+// document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="./program.html">  Program 				 </a></li>');
+// document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="./artistic program.html">  Artistic Program 				 </a></li>');
+// document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a  href ="./presenter instructions.html"> Presentation instructions</a></li>');  
+// document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="blue"/></svg> &nbsp;<a  href ="./registration.html"> Registration       </a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a  href ="./Important Dates.html">    Important dates & registration       <a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="./Author instructions & Submission.html">   Author instructions & Submission</a></li>');
-document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="./Conference venue.html">   Conference venue      </a></li>');
+// document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="./Conference venue.html">   Conference venue & accommodation     </a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="red"/></svg> &nbsp;<a  href ="./Accommodation.html"> Accommodation     </a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="yellow"/></svg> &nbsp;<a  href ="./history.html"> Tenor History</a></li>');
+document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="orange"/></svg> &nbsp;<a  href ="./sponsors.html">     Call for sponsors</a></li>');
 document.write('<li> <svg class="bullet" width="20" height="30"><circle cx="10" cy="20" r="7"  fill="green"/></svg> &nbsp;<a href ="./contact.html">  Contact us            </a></li>');
 document.write('</ul>');
