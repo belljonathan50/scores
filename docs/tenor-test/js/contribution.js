9a1bbfb6e931a801bb662d87b3847c4b4bcee86e
belljonathan50 <belljonathan50@gmail.com>
2021-08-31 11:35:54 +0200

    Update contribution.js

--- docs/tenor-test/js/contribution.js
+++ docs/tenor-test/js/contribution.js
@@ -1,5 +1,5 @@
 // document.write('<div class = "titre">latest news</div><br />');
-// document.write('<p>    <a id="cv" href= http://localhost:8000/keynotes.html> Craig Vear Keynote </a>    </p>');
+// document.write('<p>    <a id="cv" href= ./keynotes.html> Craig Vear Keynote </a>    </p>');
 
 
 document.write('<video autoplay muted loop id="hope">');
